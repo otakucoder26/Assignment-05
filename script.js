c70072433ed4fbd2f8ed71fd84f1fb575c556d6b
otakucoder26 <khanrahmatullah2606@gmail.com>
2025-08-29 11:35:34 +0600

    New commit message

---
+++ script.js
@@ -0,0 +1,10 @@
+// Reusable Function
+function getInnerText(id){
+    const element = document.getElementById(id).innerText;
+    return element;
+}
+
+function getId(id){
+    const element = document.getElementById(id);
+    return element;
+}
